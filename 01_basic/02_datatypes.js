f246327c19c1972a12e32df6ad27990db4c9c134
Ashish Kumar Pamdey <147474800+AshishPandey04@users.noreply.github.com>
2024-04-01 06:57:25 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,50 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3)
+// it will not run in nodejs , since it need brower to run .
+
+// code readability should be high 
+// write every line of code in next line 
+
+let Name = "ashish"
+let age= 21
+let isLoggedIn= false
+let state ; 
+let a=null
+let b = 123452123541312331333234244445524 
+
+
+// detatypes in javascript
+//  1)number 
+//  2)bigint 
+
+
+//  3)String =""
+// The String type is the set of all ordered sequences of zero or more 16-bit unsigned integer values (“elements”) up to a maximum length of 2**53 - 1 elements
+
+
+// 4) boolean - true or false
+// 5)null = standalone value
+
+
+// 6) undefined 
+// The Undefined type has exactly one value, called undefined. Any variable that has not been assigned a value has the value undefined.
+
+
+// 7)symbol => each possible symbol value is  always unique 
+
+//8)Object
+
+
+console.log(typeof "ashish")
+console.log(typeof age)
+console.log(typeof 23)
+console.log(typeof Name)
+console.log(typeof null) // object type
+console.log(typeof undefined) // undefined
+console.log(typeof state)
+console.log(typeof a)
+console.log(typeof isLoggedIn)
+console.log(typeof b) // bigint also is a NUMBER type 
+
+
