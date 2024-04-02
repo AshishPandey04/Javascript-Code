ba5935b8c3d08723c47ae7c9fbda8608d1239289
Ashish Kumar Pamdey <147474800+AshishPandey04@users.noreply.github.com>
2024-04-02 16:43:08 +0000

    Conversion of datatype

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "33abc"
+console.log(typeof score);
+// console.log(typeof(score));  can also write like this , in this case it is called method as it is written b/w parenthesis.
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33"=> 33
+//"33abc"=>NaN(not a number)
+//true=>1;false=>0
+//undefined=>NaN
+// null=>0
+
+
+let isLoggedIn = "ashish"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+console.log( booleanIsLoggedIn)
+
+// 1=> true ; 0=>false
+//""=> false
+//"ashish"=> true
+
+let someNumber= 33
+let stringNumber= String(someNumber)
+console.log(typeof stringNumber)
+console.log( stringNumber)
+
