4792711e114bce73cc742d6bc4dd197f18b62284
Ashish Kumar Pamdey <147474800+AshishPandey04@users.noreply.github.com>
2024-04-10 10:50:59 +0000

    Operations

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,10 +1,10 @@
-let score = "33abc"
-console.log(typeof score);
+// let score = "33abc"
+// console.log(typeof score);
 // console.log(typeof(score));  can also write like this , in this case it is called method as it is written b/w parenthesis.
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33"=> 33
 //"33abc"=>NaN(not a number)
@@ -13,18 +13,54 @@ console.log(valueInNumber);
 // null=>0
 
 
-let isLoggedIn = "ashish"
+// let isLoggedIn = "ashish"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(typeof booleanIsLoggedIn)
-console.log( booleanIsLoggedIn)
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+// console.log( booleanIsLoggedIn)
 
 // 1=> true ; 0=>false
 //""=> false
 //"ashish"=> true
 
-let someNumber= 33
-let stringNumber= String(someNumber)
-console.log(typeof stringNumber)
-console.log( stringNumber)
+// let someNumber= 33
+// let stringNumber= String(someNumber)
+// console.log(typeof stringNumber)
+// console.log( stringNumber)
+
+// ****************Operations*********************
+
+// let value=3
+// let negValue= -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); //power
+// console.log(6/1);
+// console.log(6%4);
+
+// let str1="hello"
+// let str2=" ashish"
+// let str3= str1+str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(+true);
+// console.log(+"");
+
+
+let num1, num2,num3
+
+num1=num2=num3=2+2
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter)
+
 
