e50a13e7ccdc91631fbf35711a926693d01a3c31
Ashish Kumar Pamdey <147474800+AshishPandey04@users.noreply.github.com>
2024-04-14 10:48:10 +0000

    string continue

--- 01_basic/06_string.js
+++ 01_basic/06_string.js
@@ -3,6 +3,39 @@ const repoCount= 50
 
 // console.log(name + repoCount + " Value");
 
-console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+// console.log(`hello my name is ${name.toUpperCase()} and my repo count is ${repoCount}`);
 
-const gameName= new String('Ashish Kumar')
+const gameName= new String('Ashish Kumar pandey')
+
+// console.log(gameName[0]);
+// console.log(gameName[6]);
+// console.log(gameName[8]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toLocaleLowerCase());
+// console.log(gameName.charAt(11));
+// console.log(gameName.indexOf('r'));
+
+// const newString = gameName.substring(0, 8)
+// console.log(newString);
+
+const anotherString = gameName.slice(-8, 10)
+// minus value piche se count hoga aur positive value samne se (starting index, end index)
+console.log(anotherString);
+
+
+const newStringOne = "     Ashish   "
+console.log(newStringOne);
+console.log(newStringOne.trim()); // spaces hta deta hain jo meaning less hain. That is starting space and end space remove kar deta hain
+
+// const url = "htpps://ashish.com/ashish%20pandey"
+// console.log(url.replace('%20', '_'));
+// console.log(url.includes('ashish'));
+// console.log(url.includes('abhi'));
+
+console.log(gameName.split(' ')) // split based on dash but it can be based on any value.
+
+
+
+
+
