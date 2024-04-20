a8905c403b85ccd017f607a214a46332c1c37316
Ashish Kumar Pamdey <147474800+AshishPandey04@users.noreply.github.com>
2024-04-20 09:21:15 +0000

    notes making

--- 01_basic/04_comparision.js
+++ 01_basic/04_comparision.js
@@ -1,18 +1,19 @@
-// console.log(2>1)
-// console.log(2>=1)
-// console.log(2==1)
-// console.log(2<1)
+console.log(2>1)
+console.log(2>=1)
+console.log(2==1)
+console.log(2<1)
 
+//do not compare different data type  becuase it gives unpredectable result 
 // console.log("2">1);
 // console.log("02">1);
 // console.log("2">"1");
-// console.log(2>"1"); 
+// // console.log(2>"1"); 
 
-//do not compare different data type  becuase it gives unpredectable result 
 
-// console.log(null>0);
-// console.log(null>=0);
-// console.log(null==0);
+
+console.log(null>0);
+console.log(null>=0);
+console.log(null==0);
 // the reason is that an equality check ==,and < > work differently from <= >=.
 // comparision convert null to a number , treating as 0, thats why null>=0 is true and null>0 is false
 
