5e6babb0db1add55fd9a50d3aea1146eff7596ff
AshishPandey04 <0203ashish2004@gmail.com>
2024-06-24 17:47:11 +0530

    function is js

--- 03_basic/01_functions.js
+++ 03_basic/01_functions.js
@@ -16,17 +16,24 @@ function sayMyName() {
 //   console.log(  num1+num2);
 
 // }
+// addTwoNumber(9,4)
+
+
+
 
 function addTwoNumber(num1, num2) {
     //   console.log(  num1+num2);
     // let result= num1+num2
     // return result
 
-    return num1 + num2
+// function mein return ke baad koi bhi code likha ho within function woh execute nahi hoga 
+// console.log("asas");
+
+    // return num1 + num2 
 }
 
-// addTwoNumber(3,4)
-const result = addTwoNumber(3, 4)
+
+// const result = addTwoNumber(3, 4)
 // console.log("result:", result);
 
 function loginUserMessage(username){
@@ -38,6 +45,16 @@ function loginUserMessage(username){
 
     return `${username} just logged in`
 }
+function loginUserMessage(username="sam"){//defualt value in this case fark nahi parta hain user username de ya na de , agr nahi dega toh sam is the username .
+    if(!username){         
+    
+        console.log("please enter a username");
+        return
+    }
+
+    return `${username} just logged in`
+}
 
 // console.log(loginUserMessage("Ashish"))
-console.log(loginUserMessage())
+
+console.log(loginUserMessage()) // jab kuch nahi pass karte hain toh undefined aata hain username , undefined ata hain
