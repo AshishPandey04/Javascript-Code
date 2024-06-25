4947e834433db31bc520fef8f0c590606a7c0043
AshishPandey04 <0203ashish2004@gmail.com>
2024-06-25 16:50:53 +0530

    function

--- 03_basic/01_functions.js
+++ 03_basic/01_functions.js
@@ -48,13 +48,57 @@ function loginUserMessage(username){
 function loginUserMessage(username="sam"){//defualt value in this case fark nahi parta hain user username de ya na de , agr nahi dega toh sam is the username .
     if(!username){         
     
-        console.log("please enter a username");
+        // console.log("please enter a username");
         return
     }
 
-    return `${username} just logged in`
+    // return `${username} just logged in`
 }
 
 // console.log(loginUserMessage("Ashish"))
 
 console.log(loginUserMessage()) // jab kuch nahi pass karte hain toh undefined aata hain username , undefined ata hain
+
+
+function calculateCartPrice(val, val2, ...num1  ){// ... is a rest operator use to take many values as input
+    // this method is use to take many prameter in function
+
+    return num1
+
+}
+
+// console.log(calculateCartPrice(200,300,400,4222));
+
+
+const user={
+    username:"ash",
+    price:211
+}
+function handleObject(anyobject){
+    
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price }`);
+
+}
+
+// handleObject(user)
+// handleObject({
+//     username:"var",
+//     price:399
+// })
+
+
+const newArray=[200, 400, 243,439]
+
+function returnSecondValue(getArray){
+
+    return getArray[1]
+
+}
+
+// console.log(returnSecondValue(newArray));
+console.log(returnSecondValue([3,2,5,2,2]));
+
+
+
+
+
