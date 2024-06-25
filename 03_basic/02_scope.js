245997fdfe05596c3920309790a52468cb7d098a
AshishPandey04 <0203ashish2004@gmail.com>
2024-06-25 21:17:41 +0530

    scope and mini hosting

--- 03_basic/02_scope.js
+++ 03_basic/02_scope.js
@@ -6,11 +6,44 @@ if(true){
     const b = 20 
     // var c= 30
     // d=32
-    console.log(a);
+    // console.log(a);
 
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
 // console.log(d);
+
+
+
+function one(){
+    const username="Ashish"
+
+    function two(){
+        const website ="youtube";
+        // console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}
+one()
+
+
+
+//*******************INTERSTING**************** */
+
+console.log(addOne(5));
+function addOne(num){
+return num+1
+}
+
+
+
+addTwo(5) //error when function in this way and tried to excess before
+const addTwo = function(num){ 
+    return num+2
+}
+
+// addTwo(5)
