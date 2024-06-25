e4fa2a07e1d0b3f084935a1a3d8afc61c674c864
AshishPandey04 <0203ashish2004@gmail.com>
2024-06-25 20:59:19 +0530

    scope

---
+++ 03_basic/02_scope.js
@@ -0,0 +1,16 @@
+
+// var c =300
+let a=300
+if(true){
+    let a=10
+    const b = 20 
+    // var c= 30
+    // d=32
+    console.log(a);
+
+}
+
+console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
