42990c1c9f9740ec8ebbd457a4e4ed194833aae2
AshishPandey04 <0203ashish2004@gmail.com>
2024-06-29 18:12:31 +0530

    for of loop

---
+++ 05_iterations/three.js
@@ -0,0 +1,28 @@
+//for of 
+
+// ["","",""]
+// [{},{},{}]
+
+// const arr=[1,2,3,4,5]
+// for (const num of arr) {
+
+//     console.log(num);  
+    
+// }
+
+// const greetings="hello world!"
+// for (const greet of greetings) {
+  
+//     console.log(`each char is ${greet}`);
+  
+// }
+
+
+
+//Maps
+
+const map=new Map()
+map.set('IN', "India")
+map.set('USA', "United State of America")
+map.set('IN', "India")// unique value hold karta hain map
+console.log(map);
