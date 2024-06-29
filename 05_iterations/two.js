6a210992c8f29b6cb2078f85550cb8fa3fed908c
AshishPandey04 <0203ashish2004@gmail.com>
2024-06-29 17:47:51 +0530

    do-while loop and while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,27 @@
+//while
+
+
+// let i = 0;
+// while (i < 10) {
+//     console.log(`value of index ix ${i}`);
+//     i += 3
+// }
+
+
+
+
+// let myArray = ['ash', 'asde', 'rueiw']
+// let j = 0;
+// while (j < myArray.length) {
+
+//     console.log(`the ${j}th index value of myArray is ${myArray[j]}`);
+//     j++
+// }
+
+// do while loop
+
+// let i =9;
+// do {
+//     console.log(i);
+//     i++
+// } while (i>=10&& i<20);
