c1d9b1ac65c1d0756196e4c1b91ac788f84fae73
AshishPandey04 <0203ashish2004@gmail.com>
2024-06-29 17:28:17 +0530

    for loop , break , continue

---
+++ 05_iterations/one.js
@@ -0,0 +1,31 @@
+// for 
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     console.log(index)
+    
+// }
+
+
+// break
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     if(element==6){
+//         console.log(`got the value 6 `);
+//         break;
+//     }
+//     console.log(element);
+// }
+
+
+// continue
+for (let i = 0; i < 10; i++) {
+    const element = i;
+    if(element==6){
+        console.log(`got the value 6 `);
+        continue // continue loop se bahar aajata hain aur direct increment par chala gya 
+
+    }
+    console.log(element);
+}
