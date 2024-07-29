4df48c8c9d413ba4c51103eefac7438be0500cc8
AshishPandey04 <0203ashish2004@gmail.com>
2024-07-29 19:37:14 +0530

    object

---
+++ 10_OOPs/object.js
@@ -0,0 +1,10 @@
+
+function print(){
+    console.log("hello World");
+}
+
+print.alt="bhagoooo"
+console.log(print.alt);
+console.log(print.prototype);
+print();
+// js mein har ek chiz object hoti Hain , ha par woh apna behavior follow karti hain but yes it can also be used as object
